dd170db2aed305cbc0b68393ed272c5a402fbf54
zainbondoq <zainbondoq@gmail.com>
2024-07-13 11:14:45 +0300

    phase one

---
+++ main.js
@@ -0,0 +1,10 @@
+let myElement = document.getElementById("my-div");
+let myTagElement = document.getElementsByTagName("p");
+let  myClassElement = document.getElementsByClassName("my-span");
+let myQueryElement = document.querySelector("#my-div");
+
+
+console.log(myElement);
+console.log(myTagElement);
+console.log(myClassElement);
+console.log(myQueryElement);
